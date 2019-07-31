a8e99724ab1733bc00f3f58c6df472eeb3bdb464
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-30 21:20:03 -0600

    Remove javascript and refactor fetch

--- src/utils/apiKey.js
+++ src/utils/apiKey.js
@@ -1,4 +1,3 @@
-let key = '3fe21041-077f-4ec9-a711-aaaf7baf3aa5'
-
+let key = 
 
 export default key
--- src/utils/fetchWords.js
+++ src/utils/fetchWords.js
@@ -1,8 +1,6 @@
-import key from './apiKey.js'
-
 export async function fetchSynonyms(word) {
     let synonyms = []
-    const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${key}`)
+    const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${process.env.VUE_APP_KEY}`)
     const result = await response.json()
     if (result[0].meta) {
       synonyms = result[0].meta.syns[0].map((synonym) => {
